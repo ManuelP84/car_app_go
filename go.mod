4073f8a8221d827924cd28167ba63931106e0540
Manuel Pineda <manuel.pineda@sofka.com.co>
2025-03-15 00:12:06 -0500

    feat: add config file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ManuelP84/car_app_go
 
 go 1.21.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port       string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+var (
+	config *Config
+	once   sync.Once
+)
+
+func LoadConfig() *Config {
+	once.Do(func() {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("error loading .env file: %s", err)
+			return
+		}
+
+		config = &Config{
+			Port:       getEnv("PORT"),
+			DBHost:     getEnv("DB_HOST"),
+			DBPort:     getEnv("DB_PORT"),
+			DBUser:     getEnv("DB_USER"),
+			DBPassword: getEnv("DB_PASSWORD"),
+			DBName:     getEnv("DB_NAME"),
+		}
+
+	})
+
+	return config
+}
+
+func getEnv(key string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	panic(fmt.Sprintf("environment variable %s is not set", key))
+}
